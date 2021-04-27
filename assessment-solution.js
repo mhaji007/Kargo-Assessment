2bd674d32d81e27e1ddefd6d5f78a952229480ce
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-04-27 18:00:14 -0400

    Add null checks

--- assessment-solution.js
+++ assessment-solution.js
@@ -5,8 +5,10 @@
 // Given an array: [3, 25, 209], print “Three,TwoFive,TwoZeroNine” into stdout.
 // Given an array: [10, 300, 5], print “OneZero,ThreeZeroZero,Five” into stdout.
 
+// Solution:
+
 const mapNumberToMorpheme = (integerArray) => {
-  // Map integers [0-9] to their phonetic equivalent (morphemes)
+  // Maps integers [0-9] to their phonetic equivalent (morphemes)
   const integerToMorpheme = new Map([
     ["0", "Zero"],
     ["1", "One"],
@@ -20,5 +22,21 @@ const mapNumberToMorpheme = (integerArray) => {
     ["9", "Nine"],
   ]);
 
- 
+  // Checks for presence of array
+  if (integerArray === undefined) {
+    return "No array provided - please supply a valid integer array";
+  }
+  // Checks for presence of valid array
+  if (integerArray === null || integerArray.length === 0) {
+    return "Null/empty array - please supply a valid integer array";
+  }
+
+  // Checks for presence of valid array type
+  if (
+    !integerArray.every(function (element) {
+      return typeof element === "number";
+    })
+  ) {
+    return "All members of the array argument must be integers";
+  }
 };
