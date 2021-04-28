1c506f132764eb5a2f9563c0f7812fd00e55d3d1
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-04-27 20:02:41 -0400

    Implement solution logic

--- assessment-solution.js
+++ assessment-solution.js
@@ -1,12 +1,10 @@
 /*
-
 Question:
 
 Convert an array of integers into an array of strings representing the phonetic equivalent of the integer.
 For example:
 Given an array: [3, 25, 209], print “Three,TwoFive,TwoZeroNine” into stdout.
 Given an array: [10, 300, 5], print “OneZero,ThreeZeroZero,Five” into stdout.
-
 */
 
 // Solution:
@@ -49,4 +47,31 @@ const mapNumberToMorpheme = () => {
   ) {
     return "All members of the array argument must be integers";
   }
+
+  // Stores phonetic equivalent of all integers
+  let morphemeArray = [];
+
+  for (let integer of integerArray) {
+    const stringInteger = integer.toString();
+
+    // Stores phonetic equivalent of given integer on each iteration
+    let morphemedInteger = "";
+
+    for (let i = 0; i < stringInteger.length; i++) {
+      if (integerToMorpheme.has(stringInteger[i])) {
+        morphemedInteger =
+          morphemedInteger + integerToMorpheme.get(stringInteger[i]);
+        if (
+          stringInteger.lastIndexOf(stringInteger[i]) ===
+            stringInteger.length - 1 &&
+          i === stringInteger.length - 1
+        ) {
+          morphemeArray.push(morphemedInteger);
+        }
+      }
+    }
+  }
+  return morphemeArray.join(",");
 };
+
+console.log(mapNumberToMorpheme());
